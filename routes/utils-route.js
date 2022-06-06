b2916029f08801e1f5e2e46cde7c268c30c93b65
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2022-06-06 09:28:28 +0630

    update

--- routes/utils-route.js
+++ routes/utils-route.js
@@ -5,8 +5,15 @@ const router = express.Router();
 
 router.post("/emit", async (req, res) => {
   try {
+    const { event, data, receivers, access_key } = req.body;
+    if (access_key != process.env.ACCESS_KEY) {
+      return res.status(401).json({
+        code: 401,
+        message: "Invalid access key",
+      });
+    }
+
     const io = getIO();
-    const { event, data, receivers } = req.body;
     if (receivers && receivers.length) {
       io.to(receivers).emit(event, data);
     } else {
